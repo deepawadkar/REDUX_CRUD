aa8beff6bdea2228c3f57fc2811997b4a4ff951c
deepa <deepawadkar2002@gmail.com>
2024-04-03 13:59:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Menu from './Component/Menu'
+import Home from './Pages/Home'
+import Create from './Pages/Create';
+import Update from './Pages/Update';
+import Pnf from './Pages/Pnf';
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Menu/>
+      <Routes>
+          <Route path={'/'} element={<Home/>} />
+          <Route path={'/create'} element={<Create/>} />
+          <Route path={'/edit'} element={<Update/>} />
+          <Route path={'/*'} element={<Pnf/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
